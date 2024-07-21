a7927fe5e873326ec5cb3618639f19166bad75b6
zhangwenxuan322 <798043150@qq.com>
2024-07-21 23:43:04 +1000

    React learning: finish chapter 8

--- react/01tut/src/App.js
+++ react/01tut/src/App.js
@@ -1,13 +1,36 @@
 import Header from './Header';
 import Content from './Content';
 import Footer from './Footer';
+import { useState } from 'react'
+
 
 function App() {
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: "One half pound bag of Coca Covered Almonds Unsalted"
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "Item 2"
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "Item 3"
+    }
+  ]);
+
   return (
     <div className="App">
-      <Header />
-      <Content />
-      <Footer />
+      <Header title="Grocery List" />
+      <Content
+        items={items}
+        setItems={setItems}
+      />
+      <Footer length={items.length} />
     </div>
   );
 }
--- react/01tut/src/Content.js
+++ react/01tut/src/Content.js
@@ -1,24 +1,6 @@
-import { useState } from 'react'
-import { FaTrashAlt } from 'react-icons/fa'
+import ItemList from './ItemList'
 
-const Content = () => {
-    const [items, setItems] = useState([
-        {
-            id: 1,
-            checked: false,
-            item: "One half pound bag of Coca Covered Almonds Unsalted"
-        },
-        {
-            id: 2,
-            checked: false,
-            item: "Item 2"
-        },
-        {
-            id: 3,
-            checked: false,
-            item: "Item 3"
-        }
-    ]);
+const Content = ({ items, setItems }) => {
 
     const handleCheck = (id) => {
         const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item)
@@ -35,28 +17,13 @@ const Content = () => {
     return (
         <main>
             {items.length ? (
-                <ul>
-                    {items.map((item) => (
-                        <li className='item' key={item.id}>
-                            <input
-                                type='checkbox'
-                                onChange={() => handleCheck(item.id)}
-                                checked={item.checked}
-                            />
-                            <label
-                                style={(item.checked) ? { textDecoration: 'line-through' } : null}
-                                onDoubleClick={() => handleCheck(item.id)}
-                            >{item.item}</label>
-                            <FaTrashAlt
-                                onClick={() => handleDelete(item.id)}
-                                role='button'
-                                tabIndex='0'
-                            />
-                        </li>
-                    ))}
-                </ul>
+                <ItemList
+                    items={items}
+                    handleCheck={handleCheck}
+                    handleDelete={handleDelete}
+                />
             ) : (
-                <p style={{ marginTop: '2rem' }}>Your list is empty.</p>
+                <p style={{ marginTop: '2rem' }}>Your list is empty</p>
             )}
         </main>
     )
