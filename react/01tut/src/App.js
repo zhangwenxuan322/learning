08ea9eaf0797d559d1f727ab9474b5b38cdba323
zhangwenxuan322 <798043150@qq.com>
2024-07-22 16:49:02 +1000

    React learning: finish chapter 9 (Controlled Inputs)

--- react/01tut/src/App.js
+++ react/01tut/src/App.js
@@ -2,33 +2,63 @@ import Header from './Header';
 import Content from './Content';
 import Footer from './Footer';
 import { useState } from 'react'
+import AddItem from './AddItem';
+import SearchItem from './SearchItem';
 
 
 function App() {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: false,
-      item: "One half pound bag of Coca Covered Almonds Unsalted"
-    },
-    {
-      id: 2,
-      checked: false,
-      item: "Item 2"
-    },
-    {
-      id: 3,
-      checked: false,
-      item: "Item 3"
-    }
-  ]);
+  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')) || []);
+
+  const [newItem, setNewItem] = useState('')
+
+  const [search, setSearch] = useState('')
+
+  const setAndSaveItems = (newItems) => {
+    setItems(newItems)
+    localStorage.setItem('shoppinglist', JSON.stringify(newItems))
+  }
+
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1
+    const myNewItem = { id, checked: false, item }
+    const listItems = [...items, myNewItem]
+    setAndSaveItems(listItems)
+  }
+
+  const handleCheck = (id) => {
+    const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item)
+    setAndSaveItems(listItems)
+  }
+
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id)
+    setAndSaveItems(listItems)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (!newItem) return;
+    addItem(newItem)
+    setNewItem('')
+  }
 
   return (
     <div className="App">
       <Header title="Grocery List" />
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
+      <SearchItem
+        search={search}
+        setSearch={setSearch}
+      />
       <Content
-        items={items}
+        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
         setItems={setItems}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
       />
       <Footer length={items.length} />
     </div>
--- react/01tut/src/Content.js
+++ react/01tut/src/Content.js
@@ -1,18 +1,6 @@
 import ItemList from './ItemList'
 
-const Content = ({ items, setItems }) => {
-
-    const handleCheck = (id) => {
-        const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item)
-        setItems(listItems)
-        localStorage.setItem('shoppinglist', JSON.stringify(listItems))
-    }
-
-    const handleDelete = (id) => {
-        const listItems = items.filter((item) => item.id !== id)
-        setItems(listItems)
-        localStorage.setItem('shoppinglist', JSON.stringify(listItems))
-    }
+const Content = ({ items, setItems, handleCheck, handleDelete }) => {
 
     return (
         <main>
