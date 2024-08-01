185c8b6c0365006bbd4b063110040b80a1edc926
zhangwenxuan322 <798043150@qq.com>
2024-08-01 22:45:05 +0800

    React learning: finish chapter 17, build the main blog functions, how to use navigate

--- react/react-blog/src/Post.js
+++ react/react-blog/src/Post.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Post = () => {
+const Post = ({ post }) => {
     return (
-        <main>
-            <h1>Post</h1>
-        </main>
+        <article className='post'>
+            <Link to={`/post/${post.id}`}>
+                <h2>{post.title}</h2>
+                <p className='postDate'>{post.datetime}</p>
+            </Link>
+            <p className='postBody'>{
+                (post.body).length <= 25 ?
+                    post.body : `${(post.body).substring(0, 25)}...`
+            }
+            </p>
+        </article>
     )
 }
 
