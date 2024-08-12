2c1a9b88169deec2d6411f256584c32a5040f1e9
zhangwenxuan322 <798043150@qq.com>
2024-08-12 21:55:04 +0800

    React learning: finish chapter 22, introduce easy-peasy to manage states

--- react/react-blog/src/EditPost.js
+++ react/react-blog/src/EditPost.js
@@ -1,17 +1,22 @@
 import React from 'react'
-import { useState, useEffect, useContext } from 'react'
+import { useEffect } from 'react'
 import { useParams, Link, useNavigate } from 'react-router-dom'
-import DataContext from './context/DataContext'
 import { format } from 'date-fns'
-import api from './api/posts'
+import { useStoreState, useStoreActions } from 'easy-peasy'
 
 const EditPost = () => {
-    const [editTitle, setEditTitle] = useState('')
-    const [editBody, setEditBody] = useState('')
-    const { posts, setPosts} = useContext(DataContext)
     const { id } = useParams()
-    const post = posts.find(post => post.id === parseInt(id))
     const navigate = useNavigate()
+
+    const editTitle = useStoreState(state => state.editTitle)
+    const editBody = useStoreState(state => state.editBody)
+
+    const editPost = useStoreActions(actions => actions.editPost)
+    const setEditTitle = useStoreActions(actions => actions.setEditTitle)
+    const setEditBody = useStoreActions(actions => actions.setEditBody)
+
+    const getPostById = useStoreState(state => state.getPostById)
+    const post = getPostById(id)
 
     useEffect(() => {
         if (post) {
@@ -20,19 +25,11 @@ const EditPost = () => {
         }
     }, [post, setEditTitle, setEditBody])
 
-    const handleEdit = async (id) => {
+    const handleEdit = (id) => {
         const datetime = format(new Date(), 'MMMM dd, yyyy pp')
         const updatedPost = { id, title: editTitle, datetime, body: editBody }
-        try {
-            const response = await api.put(`/posts/${id}`, updatedPost)
-            const updatedPosts = posts.map(post => post.id === id ? { ...response.data } : post)
-            setPosts(updatedPosts)
-            setEditTitle('')
-            setEditBody('')
-            navigate(`/`)
-        } catch (err) {
-            console.log(`Error: ${err.message}`)
-        }
+        editPost(updatedPost)
+        navigate(`/post/${id}`)
     }
 
     return (
@@ -57,7 +54,7 @@ const EditPost = () => {
                             onChange={(e) => setEditBody(e.target.value)}
                         />
                         <button
-                            type='submit'
+                            type='button'
                             onClick={() => handleEdit(post.id)}
                         >Submit</button>
                     </form>
--- react/react-blog/src/PostPage.js
+++ react/react-blog/src/PostPage.js
@@ -1,24 +1,17 @@
 import React from 'react'
 import { useParams, Link, useNavigate } from 'react-router-dom'
-import { useContext } from 'react'
-import DataContext from './context/DataContext'            
-import api from './api/posts'
+import { useStoreState, useStoreActions } from 'easy-peasy'
 
 const PostPage = () => {
-    const { posts, setPosts } = useContext(DataContext)
+    const deletePost = useStoreActions(actions => actions.deletePost)
+    const getPostById = useStoreState(state => state.getPostById)
     const { id } = useParams()
-    const post = posts.find(post => (post.id).toString() === id)
+    const post = getPostById(id)
     const navigate = useNavigate()
 
-    const handleDelete = async (id) => {
-        try {
-            await api.delete(`/posts/${id}`)
-            const postsList = posts.filter(post => post.id !== id)
-            setPosts(postsList)
-            navigate('/')
-        } catch (err) {
-            console.log(`Error: ${err.message}`)
-        }
+    const handleDelete = (id) => {
+        deletePost(id)
+        navigate('/')
     }
 
     return (
