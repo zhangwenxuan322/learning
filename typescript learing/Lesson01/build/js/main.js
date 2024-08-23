3f86d9b3f3b16d723bbc9095b869018b2683e3a7
zhangwenxuan322 <798043150@qq.com>
2024-08-23 13:23:28 +0800

    TypeScript learning: how to config

---
+++ typescript learing/Lesson01/build/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+let username = 'Oscar';
+console.log(username);
+let a = 12;
+let b = 6;
+let c = 2;
+console.log(a / c);
+console.log(c * b);
---
+++ typescript learing/Lesson01/src/main.ts
@@ -0,0 +1,9 @@
+let username = 'Oscar'
+console.log(username)
+
+let a: number = 12
+let b: number = 6
+let c: number = 2
+
+console.log(a / c)
+console.log(c * b)
